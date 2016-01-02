3fcc7aa17b7c1d96350f60cea29fe65f7aae4116
Bart Römgens <bart.romgens@gmail.com>
2016-01-02 23:55:09 +0100

    Code cleanup

--- contour_plot.py
+++ contour_plot.py
@@ -51,54 +51,39 @@ def create_contour_plot(stations, filename):
     np.set_printoptions(3, threshold=100, suppress=True)  # .3f
 
     delta = 0.01
-    NCONTOURS = 30
+    n_contours = 30
 
     delta_deg = 6
     lonmin = 3.0
     latmin = 50.5
     lonmax = lonmin + delta_deg
     latmax = latmin + delta_deg / 2
+
     cycle_speed_kmh = 18.0
-
-    lonrange = np.arange(lonmin, lonmax, delta)
-    latrange = np.arange(latmin, latmax, delta / 2.0)
-
-    Z = np.zeros((int(lonrange.shape[0]), int(latrange.shape[0])))
-
-    gps = utilgeo.GPS()
-    alt = 0.0
+    n_nearest = 20  # check N nearest stations as best start for cycle route
 
     print('starting spatial interpolation')
 
-    n_stations = len(stations)
-    x_known = np.zeros(n_stations)
-    y_known = np.zeros(n_stations)
-    z_known = np.zeros(n_stations)
-    values = np.zeros(n_stations)
-
-    i = 0
+    altitude = 0.0
+    gps = utilgeo.GPS()
     positions = []
-    for station in stations:
-        lon = station.lon
-        lat = station.lat
-        travel_time = station.travel_time_min
-        x, y, z = gps.lla2ecef([lat, lon, alt])
-        positions.append([x, y, z])
-        x_known[i] = x
-        y_known[i] = y
-        z_known[i] = z
-        values[i] = travel_time
-        i += 1
+    lonrange = np.arange(lonmin, lonmax, delta)
+    latrange = np.arange(latmin, latmax, delta / 2.0)
+    Z = np.zeros((int(lonrange.shape[0]), int(latrange.shape[0])))
 
+    for station in stations:
+        x, y, z = gps.lla2ecef([station.lat, station.lon, altitude])
+        positions.append([x, y, z])
+
+    # tree to find nearest neighbors
     tree = KDTree(positions)
-    print(tree.data)
 
     for i, lat in enumerate(latrange):
         if i % (len(latrange) / 10) == 0:
             print(str(int(i / len(latrange) * 100)) + '%')
         for j, lon in enumerate(lonrange):
-            x, y, z = gps.lla2ecef([lat, lon, alt])
-            distances, indexes = tree.query([x, y, z], 20)
+            x, y, z = gps.lla2ecef([lat, lon, altitude])
+            distances, indexes = tree.query([x, y, z], n_nearest)
             min_travel_time = sys.float_info.max
             for distance, index in zip(distances, indexes):
                 if stations[index].travel_time_min == 0:
@@ -109,15 +94,13 @@ def create_contour_plot(stations, filename):
             Z[i][j] = min_travel_time
 
     print('finished spatial interpolation')
-    print(Z)
 
     figure = plt.figure()
-    levels = np.linspace(0, 170, num=NCONTOURS)
-    # cs = plt.contour(lonrange, latrange, Z, levels=levels, cmap=plt.cm.jet, norm = LogNorm())
+    levels = np.linspace(0, 170, num=n_contours)
     # contours = plt.contourf(lonrange, latrange, Z, levels=levels, cmap=plt.cm.plasma)
     contours = plt.contour(lonrange, latrange, Z, levels=levels, cmap=plt.cm.jet)
     plt.colorbar(contours, format='%.1f')
-    plt.savefig('./data/contour_example.png', dpi=300)
+    plt.savefig('./data/contour_example.png', dpi=100)
     contour_to_json(contours, filename)
 
 
