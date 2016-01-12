213e0b950430bb26144c977c466b45c784c13a71
Bart Römgens <bart.romgens@gmail.com>
2016-01-12 20:45:20 +0100

    Use multiple processes for the spatial interpolation
    Uses multiprocessing Process and Queue.
    Divides the total area in several regions and processes the regions by
    individual processes. The resulting data is combinated into a single matrix for the contour plot.

--- contour_plot.py
+++ contour_plot.py
@@ -3,10 +3,18 @@ import sys
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+from multiprocessing import Process, Queue, Pool
 import utilgeo
 from scipy.spatial import KDTree
+from scipy import ndimage
 
 from contour_to_json import contour_to_json
+
+
+class ContourData:
+    def __init__(self):
+        self.Z = None
+        self.index_begin = 0
 
 
 class Station(object):
@@ -77,22 +85,35 @@ def create_contour_plot(stations, filename):
     # tree to find nearest neighbors
     tree = KDTree(positions)
 
-    for i, lat in enumerate(latrange):
-        if i % (len(latrange) / 10) == 0:
-            print(str(int(i / len(latrange) * 100)) + '%')
-        for j, lon in enumerate(lonrange):
-            x, y, z = gps.lla2ecef([lat, lon, altitude])
-            distances, indexes = tree.query([x, y, z], n_nearest)
-            min_travel_time = sys.float_info.max
-            for distance, index in zip(distances, indexes):
-                if stations[index].travel_time_min < 0.0:
-                    continue
-                travel_time = stations[index].travel_time_min + distance / 1000.0 / cycle_speed_kmh * 60.0
-                if travel_time < min_travel_time:
-                    min_travel_time = travel_time
-            Z[i][j] = min_travel_time
+    queue = Queue()
+    processes = []
+    for i in range(0, 4):
+        begin = i * len(latrange)/4
+        end = (i+1)*len(latrange)/4
+        latrange_part = latrange[begin:end]
+        process = Process(target=interpolate_travel_time, args=(queue, i, tree, gps, latrange_part, lonrange, altitude, n_nearest, cycle_speed_kmh))
+        processes.append(process)
+
+    for process in processes:
+        process.start()
+
+    # get from the queue and append the values
+    for i in range(0, 4):
+        data = queue.get()
+        index_begin = data.index_begin
+        begin = index_begin*len(latrange)/4
+        end = (index_begin+1)*len(latrange)/4
+        Z[0:][begin:end] = data.Z
+
+    for process in processes:
+        process.join()
 
     print('finished spatial interpolation')
+
+    # zoomFactor = 1
+    # Z = ndimage.zoom(Z, zoomFactor)
+    # lonrange = ndimage.zoom(lonrange, zoomFactor)
+    # latrange = ndimage.zoom(latrange, zoomFactor)
 
     figure = plt.figure()
     ax = figure.add_subplot(111)
@@ -104,9 +125,36 @@ def create_contour_plot(stations, filename):
     contour_to_json(contours, filename)
 
 
+def interpolate_travel_time(q, position, kdtree, gps, latrange, lonrange, altitude, n_nearest, cycle_speed_kmh):
+    print('interpolate_travel_time')
+    Z = np.zeros((int(latrange.shape[0]), int(lonrange.shape[0])))
+    for i, lat in enumerate(latrange):
+        if i % (len(latrange) / 10) == 0:
+            print(str(int(i / len(latrange) * 100)) + '%')
+
+        for j, lon in enumerate(lonrange):
+            x, y, z = gps.lla2ecef([lat, lon, altitude])
+            distances, indexes = kdtree.query([x, y, z], n_nearest)
+            min_travel_time = sys.float_info.max
+            for distance, index in zip(distances, indexes):
+                if stations[index].travel_time_min < 0.0:
+                    continue
+                travel_time = stations[index].travel_time_min + distance / 1000.0 / cycle_speed_kmh * 60.0
+                if travel_time < min_travel_time:
+                    min_travel_time = travel_time
+            Z[i][j] = min_travel_time
+    data = ContourData()
+    data.index_begin = position
+    data.Z = Z
+    q.put(data)
+    print('end interpolate_travel_time')
+    return
+
+
 if __name__ == "__main__":
+    departure_station = 'utrecht'
     stations = Station.from_json('./data/stations.json')
-    Station.travel_times_from_json(stations, './data/traveltimes_from_delft.json')
+    Station.travel_times_from_json(stations, './data/traveltimes_from_' + departure_station + '.json')
 
     # stations = []
     # stations.append(Station('Utrecht Centraal', 5.11027765274048, 52.0888900756836, 100))
@@ -116,4 +164,4 @@ if __name__ == "__main__":
 
     for station in stations:
         print(station)
-    create_contour_plot(stations, './data/contours_delft.json')
+    create_contour_plot(stations, './data/contours_' + departure_station + '.json')
