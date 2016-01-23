bd3783357f5285e62ab381d0ac6e2d94ff85cf7e
Bart Römgens <bart.romgens@gmail.com>
2016-01-23 16:49:31 +0100

    Include station name in stations data

--- station.py
+++ station.py
@@ -2,7 +2,8 @@ import json
 
 
 class Station(object):
-    def __init__(self, name, lon, lat, travel_time_min=-1.0):
+    def __init__(self, id, name, lon, lat, travel_time_min=-1.0):
+        self.id = id
         self.name = name
         self.lon = float(lon)
         self.lat = float(lat)
@@ -17,7 +18,7 @@ class Station(object):
         with open(filename) as file:
             stations = json.load(file)['stations']
             for station in stations:
-                stations_new.append(Station(station['names']['long'], station['lon'], station['lat']))
+                stations_new.append(Station(station['id'], station['names']['long'], station['lon'], station['lat']))
         return stations_new
 
     @staticmethod
