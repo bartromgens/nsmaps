3160b8cc8a31af1aaf7427042193d36d3ea0f2b6
Bart Römgens <bart.romgens@gmail.com>
2016-01-24 21:08:26 +0100

    Require contour data to be available for station selection

--- update_station_data.py
+++ update_station_data.py
@@ -13,12 +13,13 @@ def update_station_data():
         # if station.country == "NL" and "Utrecht" in station.names['long']:
         if station.country == "NL":
             travel_times_available = os.path.exists('./data/traveltimes_from_' + station.code + '.json')
+            contour_avaiable = os.path.exists('./data/contours_' + station.code + '.json')
             data['stations'].append({'names': station.names,
                                      'id': station.code,
                                      'lon': station.lon,
                                      'lat': station.lat,
                                      'type': station.stationtype,
-                                     'travel_times_available': travel_times_available})
+                                     'travel_times_available': travel_times_available and contour_avaiable})
 
     json_data = json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
     with open('./data/stations.json', 'w') as fileout:
