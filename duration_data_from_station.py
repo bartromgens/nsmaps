c11a0b0ab606befe88ef5a6f02383d86d76c37a6
Bart Römgens <bart.romgens@gmail.com>
2016-01-25 23:19:29 +0100

    Create function to re-create travel time data with missing stations

--- duration_data_from_station.py
+++ duration_data_from_station.py
@@ -16,7 +16,35 @@ def main():
     stations = nsapi.get_stations()
     major_stations = get_major_stations(stations)
     for major_station in major_stations:
-        create_trip_data_from_station(major_station, stations)
+        filename_out = './data/traveltimes_from_' + major_station.code + '.json'
+        if os.path.exists(filename_out):
+            logger.warning('File ' + filename_out + ' already exists. Will not overwrite by default. Return.')
+            continue
+        json_data = create_trip_data_from_station(major_station, stations)
+        with open(filename_out, 'w') as fileout:
+            fileout.write(json_data)
+
+
+def recreate_missing_destinations():
+    nsapi = ns_api.NSAPI(USERNAME, APIKEY)
+    stations = nsapi.get_stations()
+    ignore_station_ids = ['HRY', 'WTM', 'KRW', 'VMW', 'RTST', 'WIJ', 'SPV', 'SPH']
+    for station in stations:
+        filename = './data/traveltimes_from_' + station.code + '.json'
+        if not os.path.exists(filename):
+            continue
+        stations_missing = get_missing_destinations(filename, stations)
+        stations_missing_filtered = []
+        for station_missing in stations_missing:
+            if station_missing.id not in ignore_station_ids:
+                stations_missing_filtered.append(stations_missing)
+                logger.info('Missing station: ' + station_missing.name)
+        if stations_missing_filtered:
+            json_data = create_trip_data_from_station(station, stations)
+            with open(filename, 'w') as fileout:
+                fileout.write(json_data)
+        else:
+            logger.info('No missing destinations for ' + station.names['long'] + ' with ' + str(len(ignore_station_ids)) + ' ignored.')
 
 
 def get_station_id(station_name, stations):
@@ -29,18 +57,13 @@ def get_station_id(station_name, stations):
 def create_trip_data_from_station(station_from, stations):
     data = {'stations': []}
 
-    timestamp = "12-01-2016 08:00"
+    timestamp = "13-01-2016 08:00"
     via = ""
 
     data['stations'].append({'name': station_from.names['long'],
                              'id': station_from.code,
                              'travel_time_min': 0,
                              'travel_time_planned': "0:00"})
-
-    filename_out = './data/traveltimes_from_' + station_from.code + '.json'
-    if os.path.exists(filename_out):
-        logger.warning('File ' + filename_out + ' already exists. Will not overwrite by default. Return.')
-        return
 
     nsapi = ns_api.NSAPI(USERNAME, APIKEY)
     for station in stations:
@@ -75,11 +98,10 @@ def create_trip_data_from_station(station_from, stations):
                                  'id': get_station_id(shortest_trip.destination, stations),
                                  'travel_time_min': shortest_trip.travel_time_min,
                                  'travel_time_planned': shortest_trip.travel_time_planned})
-        time.sleep(0.3)  # balance load on the NS server
+        # time.sleep(0.3)  # balance load on the NS server
 
     json_data = json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
-    with open(filename_out, 'w') as fileout:
-        fileout.write(json_data)
+    return json_data
 
 
 def get_major_stations(stations):
@@ -96,7 +118,18 @@ def get_major_stations(stations):
     return major_stations
 
 
+def get_missing_destinations(filename_json, stations):
+    stations = Station.from_json()
+    Station.travel_times_from_json(stations,  filename_json)
+    missing_stations = []
+    for station in stations:
+        if station.travel_time_min == None:
+            missing_stations.append(station)
+    return missing_stations
+
+
 if __name__ == "__main__":
+    # recreate_missing_destinations()
     main()
 
     # station_from_id = "UT"  # example: Utrecht Centraal
