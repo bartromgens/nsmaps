399460a2307b1ded9dd71c3ddd8f29a0dc9b89f7
Bart Römgens <bart.romgens@gmail.com>
2016-02-07 14:49:20 +0100

    Create first test case

---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+
+import ns_api
+
+from local_settings import USERNAME, APIKEY
+
+
+class TestNSApi(unittest.TestCase):
+
+    def setUp(self):
+        self.nsapi = ns_api.NSAPI(USERNAME, APIKEY)
+
+    def testTripStopWithoutTime(self):
+        timestamp = "04-02-2016 08:00"
+        start = "Rotterdam Blaak"
+        via = ""
+        destination = "Amsterdam Centraal"
+        trips = self.nsapi.get_trips(timestamp, start, via, destination)
+
+
+if __name__ == '__main__':
+    unittest.main()
