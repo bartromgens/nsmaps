be18b14e48e6fe7ed912c8c14b0dfd38a55572ac
Bart Römgens <bart.romgens@gmail.com>
2016-02-11 23:15:55 +0100

    Create initial setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='nsmaps',
+    version='0.1dev',
+    packages=['nsmaps',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
