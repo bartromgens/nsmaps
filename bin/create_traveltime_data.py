50dcc27d931404963a7f7cda7174328dc0dcf44d
Bart Römgens <bart.romgens@gmail.com>
2016-02-28 23:23:38 +0100

    Get travel time data in batches of 60 stations
    NS API has a max number of requests per day.

--- bin/create_traveltime_data.py
+++ bin/create_traveltime_data.py
@@ -10,19 +10,34 @@ from nsmaps.station import StationType
 
 DATA_DIR = './website/data'
 
+MAX_STATIONS = 60
+
 
 def main():
-    os.path
     stations = nsmaps.station.Stations(DATA_DIR)
     major_station_types = (
         StationType.intercitystation,
         StationType.knooppuntIntercitystation,
         StationType.megastation,
-        # StationType.knooppuntSneltreinstation,
-        # StationType.sneltreinstation,
+        StationType.knooppuntSneltreinstation,
+        StationType.sneltreinstation,
+        StationType.knooppuntStoptreinstation,
+        StationType.stoptreinstation
     )
-    major_stations = stations.get_stations_for_types(major_station_types)
-    stations.create_traveltimes_data(major_stations)
+    stations_options = stations.get_stations_for_types(major_station_types)
+
+    stations_todo = []
+
+    n_stations = 0
+    for station in stations_options:
+        if n_stations >= MAX_STATIONS:
+            break
+        if not station.has_travel_time_data() and station.get_country_code() == 'NL':
+            stations_todo.append(station)
+            n_stations += 1
+            print(station)
+
+    stations.create_traveltimes_data(stations_todo)
     stations.recreate_missing_destinations(DATA_DIR, False)
 
 
