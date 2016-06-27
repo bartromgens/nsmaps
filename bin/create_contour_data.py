16a79df5303b2f9276e8711570c9a1caf464ab46
Bart Römgens <bart.romgens@gmail.com>
2016-06-27 23:36:40 +0200

    Use 2 contour layers instead of 3

--- bin/create_contour_data.py
+++ bin/create_contour_data.py
@@ -20,30 +20,23 @@ def test():
 
     filepaths = []
 
-    # test_config = nsmaps.contourmap.ContourPlotConfig()
-    test_config = nsmaps.contourmap.TestConfig()
+    test_config = nsmaps.contourmap.ContourPlotConfig()
+    # test_config = nsmaps.contourmap.TestConfig()
     test_config.print_bounding_box()
-    contourmap = nsmaps.contourmap.Contour(departure_station, stations, test_config, DATA_DIR)
-    filepath = os.path.join(DATA_DIR, 'contours/' + departure_station.get_code() + '.geojson')
-    filepaths.append(filepath)
-    contourmap.create_contour_data(filepath, min_zoom=9, max_zoom=12, stroke_width=3)
 
-    # test_major_config = nsmaps.contourmap.ContourPlotConfig(n_contours=11)
-    test_major_config = nsmaps.contourmap.TestConfig(n_contours=11)
-    contourmap_major = nsmaps.contourmap.Contour(departure_station, stations, test_major_config, DATA_DIR)
+    contourmap = nsmaps.contourmap.Contour(departure_station, stations, test_config, DATA_DIR)
+
     filepath = os.path.join(DATA_DIR, 'contours/' + departure_station.get_code() + '_major.geojson')
     filepaths.append(filepath)
-    contourmap_major.create_contour_data(filepath, min_zoom=0, max_zoom=12, stroke_width=9)
+    contourmap.create_contour_data(filepath)
+    contourmap.create_geojson(filepath, min_zoom=0, max_zoom=12, stroke_width=9, n_contours=11)
 
-    # test_minor_config = nsmaps.contourmap.ContourPlotConfig(n_contours=81)
-    test_minor_config = nsmaps.contourmap.TestConfig(n_contours=81)
-    contourmap_minor = nsmaps.contourmap.Contour(departure_station, stations, test_minor_config, DATA_DIR)
     filepath = os.path.join(DATA_DIR, 'contours/' + departure_station.get_code() + '_minor.geojson')
     filepaths.append(filepath)
-    contourmap_minor.create_contour_data(filepath, min_zoom=12, max_zoom=12, stroke_width=1)
+    contourmap.create_geojson(filepath, min_zoom=10, max_zoom=12, stroke_width=3, n_contours=51)
 
     tile_dir = os.path.join(DATA_DIR, 'contours/' + departure_station.get_code() + '/tiles/')
-    contourmap_major.create_geojson_tiles(filepaths, tile_dir=tile_dir, min_zoom=0, max_zoom=12)
+    contourmap.create_geojson_tiles(filepaths, tile_dir=tile_dir, min_zoom=0, max_zoom=12)
 
 
 def create_all():
