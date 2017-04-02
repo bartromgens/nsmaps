f6ecebb508a6b84a41c4c95dcec6e6ded026c11b
Bart Römgens <bart.romgens@gmail.com>
2017-04-02 20:49:45 +0200

    create travel time data for all stations

--- bin/create_traveltime_data.py
+++ bin/create_traveltime_data.py
@@ -14,15 +14,15 @@ MAX_STATIONS = 60
 
 
 def main():
-    stations = nsmaps.station.Stations(DATA_DIR, test=True)
+    stations = nsmaps.station.Stations(DATA_DIR, test=False)
     major_station_types = (
         StationType.megastation,
-        # StationType.knooppuntIntercitystation,
-        # StationType.intercitystation,
-        # StationType.knooppuntSneltreinstation,
-        # StationType.sneltreinstation,
-        # StationType.knooppuntStoptreinstation,
-        # StationType.stoptreinstation
+        StationType.knooppuntIntercitystation,
+        StationType.intercitystation,
+        StationType.knooppuntSneltreinstation,
+        StationType.sneltreinstation,
+        StationType.knooppuntStoptreinstation,
+        StationType.stoptreinstation
     )
     stations_options = stations.get_stations_for_types(major_station_types)
 
@@ -30,6 +30,7 @@ def main():
 
     n_stations = 0
     for station in stations_options:
+        print(station.get_name())
         if n_stations >= MAX_STATIONS:
             break
         if not station.has_travel_time_data() and station.get_country_code() == 'NL':
@@ -40,7 +41,7 @@ def main():
 
     timestamp = "19-04-2017 08:00"
     stations.create_traveltimes_data(stations_todo, timestamp)
-    stations.recreate_missing_destinations(timestamp, False)
+    # stations.recreate_missing_destinations(timestamp, False)
 
 
 if __name__ == "__main__":
